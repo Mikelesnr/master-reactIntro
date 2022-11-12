f8c2ea80f2d23d661f424f67aea1e1ecac2c6c81
Mikelesnr <mwanza.n.m@gmail.com>
2022-11-12 09:34:30 +0200

    State

--- src/Clscom.js
+++ src/Clscom.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Detail from './Detail';
+import Detail2 from './Detail'
 
 class Clscom extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { color: '#4cb96b' };
+        this.state = {
+            color: '#4cb96b',
+            laptops: [
+                { ram: "128 GB", hdd: "2 Tb", processor: "core i5" },
+                { ram: "64 GB", hdd: "1.5 Tb", processor: "core i3" },
+                { ram: "256 GB", hdd: "5 Tb", processor: "core i7" }
+            ],
+            desktops: [
+                { ram: "128 GB", gc: "4 gb", hdd: "2 Tb", processor: "core i5" },
+                { ram: "64 GB", gc: "8 gb", hdd: "1.5 Tb", processor: "core i3" },
+                { ram: "256 GB", gc: "16 gb", hdd: "5 Tb", processor: "core i7" }
+            ]
+        };
     }
+
     render() {
         return (
             <div>
@@ -21,7 +35,12 @@ class Clscom extends React.Component {
                 <p>
                     some props, {this.props.contentdata}
                 </p>
-                <Detail hdd="520Gb" ram="16B" processor="core i3" />
+                <Detail hdd={this.state.laptops[2].hdd} ram={this.state.laptops[2].ram} processor={this.state.laptops[2].processor} />
+                <Detail hdd={this.state.laptops[0].hdd} ram={this.state.laptops[0].ram} processor={this.state.laptops[0].processor} />
+                <Detail hdd={this.state.laptops[1].hdd} ram={this.state.laptops[1].ram} processor={this.state.laptops[1].processor} />
+                <Detail2 hdd={this.state.desktops[2].hdd} ram={this.state.desktops[2].ram} gc={this.state.desktops[2].gc} processor={this.state.desktops[2].processor} />
+                <Detail2 hdd={this.state.desktops[0].hdd} ram={this.state.desktops[0].ram} gc={this.state.desktops[0].gc} processor={this.state.desktops[0].processor} />
+                <Detail2 hdd={this.state.desktops[1].hdd} ram={this.state.desktops[1].ram} gc={this.state.desktops[1].gc} procesdesk={this.state.desktops[1].processor} />
             </div>
         )
     }
--- src/Detail.js
+++ src/Detail.js
@@ -5,11 +5,24 @@ class Detail extends React.Component {
         return (
             <div>
                 <p>
-                    I have a laptop with {this.props.ram} Ram, {this.props.hdd} Drive and {this.props.processor} Processor
+                    I have a laptop with {this.props.ram} Ram, {this.props.hdd} Drive and {this.props.processor} Processor.
                 </p>
             </div>
         )
     }
 };
 
-export default Detail;
+class Detail2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    I have a desktop with {this.props.ram} Ram, {this.props.gc} Graphics card, {this.props.hdd} Drive and {this.props.processor} Processor.
+                </p>
+            </div>
+        )
+    }
+};
+
+
+export default (Detail, Detail2);
